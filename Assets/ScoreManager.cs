a6e7603d79932effd37bb305d6c4e476ee39bdca
Ellaijemugbegabari <97686631+Ellaijemugbegabari@users.noreply.github.com>
2023-01-16 11:22:07 +0100

    point system 2/2

--- Assets/ScoreManager.cs
+++ Assets/ScoreManager.cs
@@ -1,18 +1,34 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
 
 public class ScoreManager : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public TextMeshProUGUI scoreText;
+   
+
+    int score = 0;
+
     void Start()
     {
+        scoreText.text = score.ToString() + " POINTS";
         
     }
 
     // Update is called once per frame
-    void Update()
+
+    // skapa en ny tag som heter "MyCoin" och lägg den på alla colliders
+
+    private void OntriggerEnter2D(Collider2D Coin)
     {
-        
+        if (Coin.tag == "MyCoin")
+        {
+            score = +1;
+            scoreText.text = "score" + score;
+        }
     }
 }
+       
